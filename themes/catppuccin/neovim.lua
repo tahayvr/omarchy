a4ce00341a5138cd25d365ba5bf1195457ee292f
Taha <paprikataha@gmail.com>
2025-07-25 17:05:27 -0700

    Add specific mocha flavor to catppuccin neovim theme

--- themes/catppuccin/neovim.lua
+++ themes/catppuccin/neovim.lua
@@ -1,8 +1,20 @@
 return {
 	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha", -- other options: "mocha", "frappe", "macchiato"
+			})
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
+	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "catppuccin",
+			colorscheme = "catppuccin-mocha",
 		},
 	},
 }
